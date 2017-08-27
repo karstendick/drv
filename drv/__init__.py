6059cf8b327123ded1d4799e1693225e6d8be741
Joshua Karstendick <joshua@stitchdata.com>
2017-08-27 19:37:49 -0400

    added pmf class

--- drv/__init__.py
+++ drv/__init__.py
@@ -1,1 +1,28 @@
-FOO = 42
+from collections import defaultdict
+from fractions import Fraction
+
+class Pmf(defaultdict):
+    """pmf is a defaultdict of int to Fraction, mapping values to
+    probabilities
+
+    """
+    def __init__(self, mapping):
+        probability_sum = sum(mapping.values())
+        if probability_sum != 1:
+            raise ValueError("Pmf sum must be 1; received invalid value {}"
+                             .format(probability_sum))
+        defaultdict.__init__(self, int, mapping)
+
+class Drv:
+    def __init__(self, pmf):
+        self._pmf = pmf
+
+
+def main():
+    pmf = Pmf({1: Fraction(1,2),
+               2: Fraction(1,2)})
+    import pdb; pdb.set_trace()
+    print('done')
+
+if __name__ == "__main__":
+    main()
--- tests/test_drv.py
+++ tests/test_drv.py
@@ -1,7 +1,14 @@
 import unittest
+from fractions import Fraction
 
 from drv import *
 
-class TestDrv(unittest.TestCase):
-    def test_the_tests(self):
-        self.assertEqual(42, FOO)
+class TestPmf(unittest.TestCase):
+    def test_probability_sum(self):
+        bad_pmf = {1: Fraction(1,2),
+                   2: Fraction(1,2),
+                   3: Fraction(1,2)}
+        self.assertRaises(ValueError, Pmf, bad_pmf)
+        good_pmf = {1: Fraction(1,2),
+                    2: Fraction(1,2)}
+        self.assertEqual(Pmf(good_pmf), Pmf(good_pmf))
