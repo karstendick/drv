a3e3c8d276347d1bb1231fe5b5415d1b95734d34
Joshua Karstendick <joshdick@gmail.com>
2018-11-17 10:50:48 -0500

    added str representation and check against negative probabilities

--- drv/__init__.py
+++ drv/__init__.py
@@ -3,6 +3,7 @@ import itertools
 from fractions import Fraction
 from math import sqrt
 
+
 class Pmf(defaultdict):
     """A Pmf (probability mass function) is a defaultdict of int to Fraction,
     mapping values to probabilities.
@@ -10,9 +11,16 @@ class Pmf(defaultdict):
     """
     def __init__(self, mapping):
         probability_sum = sum(mapping.values())
+
         if probability_sum != 1:
             raise ValueError("Pmf sum must be 1; received invalid value {}"
                              .format(probability_sum))
+
+        neg_probs = {value: prob for value, prob in mapping.items() if prob < 0}
+        if neg_probs:
+            raise ValueError("Probabilities must be non-negative; received invalid values {}"
+                            .format(neg_probs))
+
         defaultdict.__init__(self, int, mapping)
 
     def support(self):
@@ -23,6 +31,14 @@ class Pmf(defaultdict):
 
     def max(self):
         return max(self.support())
+
+    def __str__(self):
+        m = {value: round(float(prob), 5) for value, prob in self.items() if prob > 0}
+        l = ["{0: >3} {1: >8.2%}".format(value, prob) for value, prob in m.items()]
+        return "\n".join(l)
+
+    def __repr__(self):
+        return self.__str__()
 
 class Drv:
     """A Drv (discrete random variable) has a Pmf and provides methods for
@@ -84,6 +100,9 @@ def multiply(x, y):
 # defaultdict(<class 'int'>, {0: Fraction(-1, 4), 1: Fraction(6, 5), 2: Fraction(1, 20)})
 # attack_pmf(30, 5)
 # defaultdict(<class 'int'>, {0: Fraction(6, 5), 1: Fraction(-1, 4), 2: Fraction(1, 20)})
+#
+# TODO: Bug: don't just double damage on a crit; instead, roll more dice
+# i.e.: 2d6, NOT 2*(1d6)
 def attack_pmf(ac, attack_mod):
     atk_pmf = Pmf({0: Fraction(ac - attack_mod -1, 20),
                     1: Fraction(20-ac + attack_mod, 20),
@@ -98,6 +117,7 @@ def attack_dmg_mod_pmf(ac, attack_mod):
     atk_pmf = Pmf({0: Fraction(ac - attack_mod -1, 20),
                     1: Fraction(20-ac + attack_mod+1, 20)})
     return atk_pmf
+
 
 def attack_dmg_pmf(ac, attack_mod, dmg_pmf, dmg_mod):
     #prob_miss = ac - attack_mod -2
@@ -115,6 +135,7 @@ def main():
     d4 = die_pmf(4)
     four_d4 = add(d4, add(d4, add(d4, d4)))
     drv = Drv(four_d4)
+    attack_roll = attack_dmg_pmf(13, 5, d6, 3)
     import pdb; pdb.set_trace()
     print('done')
 
