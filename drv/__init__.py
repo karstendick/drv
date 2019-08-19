9daae0460dc53ead915f010ab59da00448742c9d
Joshua Karstendick <joshdick@gmail.com>
2019-08-18 21:13:53 -0400

    plot weapons together

--- drv/__init__.py
+++ drv/__init__.py
@@ -55,17 +55,44 @@ def pr_hit(ac, attack_mod):
 def pr_miss(ac, attack_mod):
     return 1 - pr_crit(ac, attack_mod) - pr_hit(ac, attack_mod)
 
+def plot_drv(drv):
+    plt.bar(drv.keys(), drv.values())
+    plt.xticks(drv.support())
+    # plt.show()
 
 def main():
-    d6 = Dice({6:1})
-    attack_roll = attack_dmg_pmf(15, 5, d6, 3)
-    print(attack_roll)
+    shortsword = Dice({6:1})
+    greataxe = Dice({12:1})
+    greatsword = Dice({6:2})
 
+    shortsword_attack_roll = attack_dmg_pmf(15, 5, shortsword, 3)
+    print(shortsword_attack_roll)
+    plt.subplot(1, 3, 1)
+    plot_drv(shortsword_attack_roll)
 
+    greataxe_attack_roll = attack_dmg_pmf(15, 5, greataxe, 3)
+    print(greataxe_attack_roll)
+    plt.subplot(1, 3, 2)
+    plot_drv(greataxe_attack_roll)
 
-    plt.bar(attack_roll.keys(), attack_roll.values())
-    plt.xticks(attack_roll.support())
+    greatsword_attack_roll = attack_dmg_pmf(15, 5, greatsword, 3)
+    print(greatsword_attack_roll)
+    plt.subplot(1, 3, 3)
+    plot_drv(greatsword_attack_roll)
+
     plt.show()
+
+
+    f, axarr = plt.subplots(3, sharex=True)
+    axarr[0].bar(shortsword_attack_roll.keys(), shortsword_attack_roll.values())
+    axarr[0].set_title('Shortsword')
+    axarr[1].bar(greataxe_attack_roll.keys(), greataxe_attack_roll.values())
+    axarr[1].set_title('Greataxe')
+    axarr[2].bar(greatsword_attack_roll.keys(), greatsword_attack_roll.values())
+    axarr[2].set_title('Greatsword')
+
+    plt.show()
+
 
     import pdb; pdb.set_trace()
     print('done')
